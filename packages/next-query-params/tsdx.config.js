376227c662e76a415e346b2eff4eda0b27d7e4ad
Jan Amann <jan@amann.me>
2021-08-31 15:46:28 +0200

    Transpile other deps too

--- packages/next-query-params/tsdx.config.js
+++ packages/next-query-params/tsdx.config.js
@@ -1,16 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const commonjs = require('@rollup/plugin-commonjs');
 
-// The `query-string` dependency is bundled so it works in IE11
+// The `query-string` dependency is bundled so it works in IE11.
+// For this to work, we also have to bundle the other dependencies
+// (except for `react` and `next` of course).
 
 module.exports = {
   rollup(config) {
     const fallbackExternal = config.external;
     config.external = (id) => {
       if (
-        ['use-query-params', 'serialize-query-params', 'query-string'].includes(
-          id
-        )
+        [
+          'use-query-params',
+          'serialize-query-params',
+          'query-string',
+          'decode-uri-component',
+          'filter-obj',
+          'split-on-first',
+          'strict-uri-encode'
+        ].includes(id)
       ) {
         return false;
       }
