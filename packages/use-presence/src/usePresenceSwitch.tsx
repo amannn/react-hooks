e558eefed007aa0f73c22505342d85264c7f880f
Jan Amann <jan@amann.work>
2023-02-10 15:08:16 +0100

    Cleanup

--- packages/use-presence/src/usePresenceSwitch.tsx
+++ packages/use-presence/src/usePresenceSwitch.tsx
@@ -1,35 +1,36 @@
 import {useState, useEffect} from 'react';
-import usePresence from './use-presence';
+import usePresence from './usePresence';
 
 export default function usePresenceSwitch<ItemType>(
-  item: ItemType,
+  item: ItemType | undefined,
   opts: Parameters<typeof usePresence>[1]
 ) {
   const [mountedItem, setMountedItem] = useState(item);
-  const [shouldBeMounted, setShouldBeMounted] = useState(!isNil(item));
-  const {isMounted, ...otherStates} = usePresence(shouldBeMounted, opts);
+  const [shouldBeMounted, setShouldBeMounted] = useState(!isUndefined(item));
+  const {isMounted, ...rest} = usePresence(shouldBeMounted, opts);
+
   useEffect(() => {
     if (mountedItem !== item) {
       if (isMounted) {
         setShouldBeMounted(false);
-      } else if (!isNil(item)) {
+      } else if (!isUndefined(item)) {
         setMountedItem(item);
         setShouldBeMounted(true);
       }
-    } else if (isNil(item)) {
+    } else if (isUndefined(item)) {
       setShouldBeMounted(false);
-    } else if (!isNil(item)) {
+    } else if (!isUndefined(item)) {
       setShouldBeMounted(true);
     }
   }, [item, mountedItem, shouldBeMounted, isMounted]);
 
   return {
-    ...otherStates,
-    isMounted: isMounted && !isNil(mountedItem),
+    ...rest,
+    isMounted: isMounted && !isUndefined(mountedItem),
     mountedItem
   };
 }
 
-function isNil<ItemType>(value: ItemType) {
-  return value == null;
+function isUndefined<ItemType>(value: ItemType) {
+  return value === undefined;
 }
--- packages/use-presence/test/usePresenceSwitch.test.tsx
+++ packages/use-presence/test/usePresenceSwitch.test.tsx
@@ -8,14 +8,14 @@ function Expander({
   transitionDuration = 50
 }: {
   initialEnter?: boolean;
-  text: string | null;
+  text?: string;
   transitionDuration?: number;
 }) {
   const {mountedItem, ...values} = usePresenceSwitch(text, {
     transitionDuration,
     initialEnter
   });
-  const {isAnimating, isMounted, isVisible} = values;
+  const {isMounted, isVisible} = values;
   const testId =
     Object.entries(values)
       .filter(([, value]) => value)
@@ -27,18 +27,12 @@ function Expander({
       {isMounted ? (
         <div
           style={{
-            overflow: 'hidden',
-            maxHeight: 0,
-            transitionDuration: `${transitionDuration}ms`,
-            transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-            transitionProperty: 'max-height, opacity',
             opacity: 0,
+            transitionDuration: `${transitionDuration}ms`,
+            transitionProperty: 'opacity',
             ...(isVisible && {
-              maxHeight: 9999,
-              opacity: 1,
-              transitionTimingFunction: 'cubic-bezier(0.8, 0, 0.6, 1)'
-            }),
-            ...(isAnimating && {willChange: 'max-height, opacity'})
+              opacity: 1
+            })
           }}
         >
           {mountedItem}
@@ -64,14 +58,16 @@ it("can animate the exit and re-entrance of a component that has changed it's re
 });
 
 it("can animate the initial entrance and exit of a component based on it's rendered data", async () => {
-  const {getByTestId, getByText, rerender} = render(<Expander text={null} />);
+  const {getByTestId, getByText, rerender} = render(
+    <Expander text={undefined} />
+  );
   getByTestId('none');
   getByText('Nothing mounted');
   rerender(<Expander text="initial value" />);
   getByTestId('isAnimating, isEntering, isMounted');
   getByText('initial value');
   await waitFor(() => getByTestId('isVisible, isMounted'));
-  rerender(<Expander text={null} />);
+  rerender(<Expander text={undefined} />);
   getByTestId('isAnimating, isExiting, isMounted');
   getByText('initial value');
   await waitFor(() => getByTestId('none'));
